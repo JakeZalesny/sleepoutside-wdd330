09d384e9edbc55a7dd66c9e8d81efd97a8f17ef0
Jake Zalesny <jrzalesny@gmail.com>
2024-05-04 11:12:58 -0600

    fixed talus tent bug

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,12 +1,14 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = [getLocalStorage("so-cart")];
+  console.log(cartItems);
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
 function cartItemTemplate(item) {
+  console.log(item.Image);
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
