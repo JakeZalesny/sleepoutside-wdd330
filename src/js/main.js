f79ac8f7d2f12125b305fb751120128bfb5d53fc
Paul Amago <apaulo2035@gmail.com>
2024-05-09 20:02:12 -0700

    added event listeners but there're still bugs

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,40 @@
 import { renderProduct } from "./dynamicProducts";
 
 // modal for first visit
-if(!localStorage.getItem("firstVisit")) {
-    var firstVisit = new Date().toISOString();
-    localStorage.setItem("firstVisit", firstVisit);
+if (!localStorage.getItem("firstVisit")) {
+  var firstVisit = new Date().toISOString();
+  localStorage.setItem("firstVisit", firstVisit);
 
-    var modal = document.querySelector("#modal");
-    modal.style.display = "block";
+  var modal = document.querySelector("#modal");
+  modal.style.display = "block";
 
-    var closeButton = document.querySelector(".close");
+  var closeButton = document.querySelector(".close");
 
-    closeButton.onclick = function() {
-        modal.style.display = "none";
-    }
-
+  closeButton.onclick = function () {
+    modal.style.display = "none";
+  };
 }
 
-// listeners for dynamic products 
+const products = await fetch("../json/products.json");
+
+// listeners for dynamic products;
+const northfaceTalusButton = document.getElementById("northface-talus");
+const cedarRidgeButton = document.getElementById("cedar-ridge");
+const marmotButton = document.getElementById("marmot-ajax");
+const northfaceAlpineButton = document.getElementById("northface-alpine");
+
+northfaceTalusButton.addEventListener("click", () => {
+  renderProduct(products[1]);
+});
+
+cedarRidgeButton.addEventListener("click", () => {
+  renderProduct(products[5]);
+});
+
+marmotButton.addEventListener("click", () => {
+  renderProduct(products[0]);
+});
+
+northfaceAlpineButton.addEventListener("click", () => {
+  renderProduct(products[3]);
+});
