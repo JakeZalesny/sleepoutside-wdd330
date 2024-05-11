b8903685adee26a2bc80ab7347a9c6f86b69b3f9
Jake Zalesny <jrzalesny@gmail.com>
2024-05-11 14:25:31 -0600

    fixed cart broken again haha

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
@@ -19,10 +19,30 @@ function cartItemTemplate(item) {
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
+  <span data-id="${item.Id}" class="cart-card__remove">X</span>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
 
   return newItem;
 }
 
+function removeProductFromCart(product) {
+  // remove product from cart
+  const cart = getLocalStorage("so-cart");
+  console.log(product);
+  const newCart = cart.filter((item) => item.Id !== product);
+  console.log(newCart);
+  setLocalStorage("so-cart", newCart);
+
+  location.reload();
+}
+
 renderCartContents();
+
+document.querySelectorAll(".cart-card__remove").forEach(button => {
+  console.log("button added", button)
+  button.addEventListener("click", function(event) {
+      const productId = event.target.getAttribute('data-id');
+      removeProductFromCart(productId);
+  });
+});
