16aab05066f306a8e81e971d0edc711d9e61b59f
Jake Zalesny <jrzalesny@gmail.com>
2024-05-10 18:45:45 -0600

    synced dynamic products

--- src/js/dynamicProducts.js
+++ src/js/dynamicProducts.js
@@ -2,25 +2,62 @@ const description = document.querySelector(".product__description");
 const color = document.querySelector(".product__color");
 const price = document.querySelector(".product-card__price");
 const image = document.querySelector("#product-image");
-const name = document.querySelector(".product__name");
+const name = document.querySelector("#product__name");
 const company = document.querySelector(".product__company");
 const title = document.querySelector("title");
 
-const products = await fetch("../json/products.json");
 
+
+const response = await fetch('../json/tents.json');
+const products = await response.json();
 // Will need to add another js file to create event listeners on main index page
 // These will run the function below
 
-export function renderProduct(product) {
+function getUrlParams() {
+    var searchParams = new URLSearchParams(window.location.search);
+    var params = {};
+    for (var [key, value] of searchParams.entries()) {
+        params[key] = value;
+    }
+    // console.log(params);
+    return params;
+}
+
+function populateContent() {
+    var params = getUrlParams();
+    var type = params["type"];
+    return type;
+
+}
+
+function renderProduct(product) {
     title.innerHTML = `Sleep Outside | ${product.Name}`
+    company.textContent = product.Brand.Name;
+    name.textContent = product.NameWithoutBrand;
     description.innerHTML = product.DescriptionHtmlSimple;
-    color.textContent = `${product.Colors[0]["colorName"]}/${product.Colors[1]["colorName"]}`;
-    price.textContent = `$${product.Price}`;
+    color.textContent = `${product.Colors[0].ColorName}`;
+    price.textContent = `$${product.FinalPrice}`;
     image.src = product.Image;
     image.alt = product.NameWithoutBrand;
-    name.textContent = product.NameWithoutBrand;
     company.textsContent = product.Brand.Name;
 
+}
+
+const type = populateContent();
+
+switch (type) {
+    case "ajax":
+            renderProduct(products[0]);   
+        break;
+    case "talus":
+        renderProduct(products[1]);   
+        break; 
+    case "cedar-ridge":
+        renderProduct(products[5]);
+        break;
+    case "alpine":
+        renderProduct(products[3]);
+        break; 
 }
 
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,3 @@
-import { renderProduct } from "./dynamicProducts";
-
 // modal for first visit
 if (!localStorage.getItem("firstVisit")) {
   var firstVisit = new Date().toISOString();
@@ -15,26 +13,28 @@ if (!localStorage.getItem("firstVisit")) {
   };
 }
 
-const products = await fetch("../json/products.json");
+// const products = await fetch("../json/products.json").then(products => products.json());
 
-// listeners for dynamic products;
-const northfaceTalusButton = document.getElementById("northface-talus");
-const cedarRidgeButton = document.getElementById("cedar-ridge");
-const marmotButton = document.getElementById("marmot-ajax");
-const northfaceAlpineButton = document.getElementById("northface-alpine");
+// // listeners for dynamic products;
+// const northfaceTalusButton = document.getElementById("northface-talus");
+// const cedarRidgeButton = document.getElementById("cedar-ridge");
+// const marmotButton = document.getElementById("marmot-ajax");
+// const northfaceAlpineButton = document.getElementById("northface-alpine");
 
-northfaceTalusButton.addEventListener("click", () => {
-  renderProduct(products[1]);
-});
+// northfaceTalusButton.addEventListener("click", () => {
+//   console.log(products[1]);
+//   renderProduct(products[1]);
+  
+// });
 
-cedarRidgeButton.addEventListener("click", () => {
-  renderProduct(products[5]);
-});
+// cedarRidgeButton.addEventListener("click", () => {
+//   renderProduct(products[5]);
+// });
 
-marmotButton.addEventListener("click", () => {
-  renderProduct(products[0]);
-});
+// marmotButton.addEventListener("click", () => {
+//   renderProduct(products[0]);
+// });
 
-northfaceAlpineButton.addEventListener("click", () => {
-  renderProduct(products[3]);
-});
+// northfaceAlpineButton.addEventListener("click", () => {
+//   renderProduct(products[3]);
+// });
