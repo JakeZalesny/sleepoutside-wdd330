b6f3fba6737bcc9ce47bae83f74e3a242289df70
Paul Amago <apaulo2035@gmail.com>
2024-05-16 10:40:03 -0600

    team 3 proj

---
+++ src/js/components/ProductList.svelte
@@ -0,0 +1,21 @@
+
+<script>
+import { getData } from "../productData.mjs";
+import ProductSummary from "./ProductSummary.svelte"
+
+export let category;
+
+let promise = getData(category);
+</script>
+
+<h2>Top products: {category}</h2>
+{#await promise}
+  Loading
+{:then products}
+
+    <ul class="product-list">
+        {#each products.slice(0,4) as product}
+            <li class="product-card"><ProductSummary {product} /></li>
+        {/each}
+    </ul>
+{/await} 
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
+import ProductList from "./components/ProductList.svelte";
+
+new ProductList({
+  target: document.querySelector(".products"),
+  props: { category: "tents" },
+});
+
 // modal for first visit
 if (!localStorage.getItem("firstVisit")) {
   var firstVisit = new Date().toISOString();
