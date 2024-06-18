9d5e8b298d31f5d41b3357f9285230e43446eb1d
Paul Amago <104170582+BowlofRocks@users.noreply.github.com>
2024-06-18 09:11:49 -0600

    fixed env stuff

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -9,9 +9,9 @@ function convertToJson(res) {
 }
 
 export async function getData(category = "tents") {
-    const response = await fetch(baseURL + `products/search/${category}`);
-    const data = await convertToJson(response);
-    return data.Result;
+  const response = await fetch(baseURL + `products/search/${category}`);
+  const data = await convertToJson(response);
+  return data.Result;
 }
 
 export async function findProductById(id) {
