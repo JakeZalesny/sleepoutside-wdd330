a52c44561933f939a2babb933043705849ca99a9
Jake Zalesny <jrzalesny@gmail.com>
2024-06-22 17:14:44 -0600

    added a wishlist feature

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -11,6 +11,7 @@ export async function productDetails(id, selector) {
     const el = document.querySelector(selector);
     el.insertAdjacentHTML("afterbegin", renderProductDetails(product));
     document.getElementById("addToCart").addEventListener("click", addToCartHandler);
+    document.getElementById("addToWishList").addEventListener("click", addToWishListHandler);
 }
 function addToCart() {
     let cartContents = getLocalStorage("so-cart");
@@ -30,6 +31,23 @@ function addToCart() {
     addToCart(product);
   }
 
+  function addToWishList() {
+    let wishList = getLocalStorage("so-wishlist");
+    if (!wishList) {
+      wishList = [];
+    }
+    wishList.push(product);
+    setLocalStorage("so-wishlist", wishList); 
+
+    // update the visible wishlistCount
+    wishlistCount.set(wishList.length);
+  }
+
+  async function addToWishListHandler(e) {
+    const product = await findProductById(e.target.dataset.id);
+    addToWishList(product);
+  }
+
   function renderProductDetails(product) {
     return `<h3>${product.Brand.Name}</h3>
     <h2 class="divider">${product.NameWithoutBrand}</h2>
@@ -45,5 +63,8 @@ function addToCart() {
     </p>
     <div class="product-detail__add">
       <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+    </div>
+    <div class="product-detail__wishlist">
+      <button id="addToWishList" data-id="${product.Id}">Add to Wish List</button>
     </div>`;
   }
